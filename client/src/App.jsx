5044c252f947852cb0dabaded9683d33ca2093ec
sanchay <kedia,sanchay@gmail.com>
2023-09-30 17:31:42 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,17 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/header';
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element ={<Home/>}/>
     <Route path="/sign-in" element ={<SignIn/>}/>
     <Route path="/sign-up" element ={<SignUp/>}/>
     <Route path="/profile" element ={<Profile/>}/>
   </Routes>
-  </BrowserRouter>;
+  </BrowserRouter>
+  )
 }
