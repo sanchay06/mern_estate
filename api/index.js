de8fc08b34f3f92b948d25afce96eccaac74725c
sanchay <kedia,sanchay@gmail.com>
2023-10-02 13:42:22 +0530

    created middleware and function to handle function based errors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO)
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
@@ -12,3 +15,14 @@ app.listen(3000, () => {
 );
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Service Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
