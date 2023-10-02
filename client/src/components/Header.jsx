b6bd48050f92095de2f708f5beaa672b9006bddf
sanchay <kedia,sanchay@gmail.com>
2023-10-02 14:29:11 +0530

    completed sign up page & functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,10 @@ export default function Header() {
                 <Link to='/about'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
                 <Link to='/sign-in'>
-                <li className=' text-slate-700 hover:underline'>SignIn</li>
+                <li className=' text-slate-700 hover:underline'>Sign In</li>
+                </Link>
+                <Link to='/sign-up'>
+                <li className=' text-slate-700 hover:underline'>Sign Up</li>
                 </Link>
             </ul>
         </div>
